507899cc9c09a6ad30d219d4d2a4314ddceae729
aanmeba <aanmeba@gmail.com>
2022-06-22 15:45:54 +1000

    creates axios setup

---
+++ src/services/memoService.js
@@ -0,0 +1,13 @@
+import memoAPI from "../config/api";
+
+export const getMemosService = async () => {
+  const response = await memoAPI.get("/memos");
+  console.log("memoService - get", response.data);
+  return response.data;
+};
+
+export const createMemoService = async (memo) => {
+  const response = await memoAPI.post("/memos", memo);
+  console.log("memoService - post", response.data);
+  return response.data;
+};
