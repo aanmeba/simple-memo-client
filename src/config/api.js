332a0d8bb98d7057dffd53faf7a1c7cf26c574a7
aanmeba <aanmeba@gmail.com>
2022-07-01 15:55:02 +1000

    sets an environment variable for backend api

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const memoAPI = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_BACKEND_BASE_URL || "http://localhost:5000",
 });
 
 export default memoAPI;
