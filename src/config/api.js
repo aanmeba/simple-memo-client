678e6d8679414387ba0607295d20673b3e0218a2
aanmeba <aanmeba@gmail.com>
2022-12-08 18:20:01 +1100

    remove console.logs

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-console.log(`**** ${process.env.REACT_APP_AXIOS_BASE_URL}`);
 const memoAPI = axios.create({
   // baseURL: "http://localhost:5000",
   baseURL: process.env.REACT_APP_AXIOS_BASE_URL || "http://localhost:5000",
